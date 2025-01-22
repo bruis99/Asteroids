438afd1009d82c0f2287836f2120ab38200323aa
bruis99 <bruis99@gmail.com>
2025-01-21 22:58:10 -0800

    Add player triangle along with hitbox of a circle with a,s,d,w movement keys

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
+import os
+os.environ["SDL_VIDEODRIVER"] = "x11"
 import pygame
 from constants import *
+from player import Player
 
 def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     black = (0,0,0)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000 #print(f"Delta time: {dt}")    
         screen.fill(black)
+        for sprite in updatable:
+            sprite.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
+        #pygame.draw.rect(screen, (255, 0, 0), (100, 100, 50, 50)) #RED SQUARE TEST ON XSERVER SCREEN
         pygame.display.flip()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+        
 if __name__ == "__main__":
     main()
